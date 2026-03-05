7a7cd796c6c3a0bd5a34d28aede9dbd36b0b0ac8
lucascastilho24 <141465068+lucascastilho24@users.noreply.github.com>
2026-03-05 12:13:58 -0300

    updade_5-3-2026

---
+++ Back-End/1oTrimestre/script.js
@@ -0,0 +1,12 @@
+var altura = 5;
+var comprimento = 9;
+area = comprimento * altura;
+const forma = "Retângulo";
+if (forma === "Retângulo") {
+    area = altura * comprimento;
+}
+else{
+    area = (altura * comprimento)/2;
+};
+
+console.log(area);
---
+++ Back-End/1oTrimestre/tipoBoolean.js
@@ -0,0 +1,9 @@
+//Booleanto
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const terceiroNumero = 5;
+const texto1 = "Colégio";
+const texto2 = "C"
+console.log(primeiroNumero===segundoNumero);
+console.log(primeiroNumero===terceiroNumero);
+console.log(texto1 === texto2);
