d4f16c1f06379380b22a2de4986e30c796af5ec9
lucascastilho24 <141465068+lucascastilho24@users.noreply.github.com>
2026-03-17 12:07:07 -0300

    update_17_3

---
+++ Back-End/1oTrimestre/Funcoes.js
@@ -0,0 +1,10 @@
+// Funçoes
+function imprimiTexto (texto) {
+    console.log(texto);
+};
+imprimiTexto("Oi!");
+imprimiTexto("Outro texto");
+function soma () {
+    const resultado = 2 + 2;
+};
+console.log(soma());
---
+++ Back-End/1oTrimestre/Template.js
@@ -0,0 +1,6 @@
+//Template
+let nome = "Daniel";
+let idade = 2026 - 2009;
+let cidade = "Curitiba";
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nascí em ${cidade}`;
+console.log(apresentacao);
