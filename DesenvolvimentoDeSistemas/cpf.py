fc653207ed0f3b932cf06e9e0453bf0f29c5513b
lucascastilho24 <141465068+lucascastilho24@users.noreply.github.com>
2026-05-25 09:38:25 -0300

    update_22_05_2-26

--- DesenvolvimentoDeSistemas/cpf.py
+++ DesenvolvimentoDeSistemas/cpf.py
@@ -1,11 +1,31 @@
-cpf = []
-entrada = "09191148901"
-print(entrada)
-for i in entrada:
-    cpf.append(int(i))
+def validar_cpf():
+    cpf = []
+    entrada = input("Digite seu CPF: ")
+    for i in entrada:
+        cpf.append(int(i))
 
-print(cpf)
-dv = cpf[-2:]
-nums = cpf[:9]
-print(dv)
-print(nums)
+    dv = cpf[-2:]
+    part1 = cpf[:9]
+
+    def calculo_dro_digito(part,peso):
+        soma = []
+        if peso == 10:
+            for i in part:
+                soma.append(int(i)*peso)
+                peso -= 1
+        else:
+            part.append(digito1)
+            for i in part:
+                soma.append(int(i)*peso)
+                peso -= 1
+
+        resto = sum(soma) % 11
+        return 0 if resto < 2 else 11- resto
+
+    digito1 = calculo_dro_digito(part1,10)
+    digito2 = calculo_dro_digito(part1,11)
+    if dv[0] == digito1 and dv[1] == digito2:
+        print(f"O CPF {entrada} é válido!")
+    else:
+        print("O CPF é inválido!")
+validar_cpf()
